b8569f371737d45c5ffb1f6bd2298d946a8b0dfa
Karmanyenn <gozdeoztokat@hotmail.com>
2022-08-09 12:01:27 +0300

    ilk commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import LoginPage from "./LoginPage";
+import './Assets/Styles/App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+export default class App extends Component {
+  
+  render() {
+    let titles={
+      loginTitle:'Giriş Yap', 
+      usernameTitle:'Kullanıcı Adı: ',
+      mailTitle:'E-posta: ',
+      passwordTitle:'Parola: ',
+      usernameLetter:'Kullanıcı adı boş bırakılamaz.',
+      mailLetter:'E-posta alanı boş bırakılamaz.',
+      pswLetter:'Parola en az 8 karakterden oluşmalıdır.'
+  
+    }
+    return (
+      <div className='app'>
+        <LoginPage loginTitles={titles} />
+      </div>
+    )
+  }
 }
 
-export default App;
+
---
+++ src/LoginPage.js
@@ -0,0 +1,105 @@
+import React, { Component } from 'react'
+import './Assets/Styles/LoginPage.css'
+import alertify from 'alertifyjs';
+
+export default class LoginPage extends Component {
+    state = { variables: [] }
+
+    componentDidMount() {
+        this.getUsers();
+    }
+    getUsers = (variable) => {
+        fetch('http://localhost:3000/variables')
+            .then(response => response.json())
+            .then(data => this.setState({ variables: data }));
+    }
+
+    onSubmitHandle = (event) => {
+        event.preventDefault();
+        this.state.variables.forEach(function (variable) {
+            var valueUser = event.target.elements.userName.value;
+            var valueMail = event.target.elements.email.value;
+            var valuePassword = event.target.elements.password.value;
+
+            var itemUser = event.target.elements.userName;
+            var itemMail = event.target.elements.email;
+            var itemPassword = event.target.elements.password;
+
+            if (valueUser === '') {
+                document.getElementById('emptyUsername').style.display = 'flex';
+                itemUser.style.border = '3px solid red';
+            }else if (valueMail === '') {
+                document.getElementById('emptyMail').style.display = 'flex';
+                itemMail.style.border = '3px solid red';
+            } else if ((valueUser === variable.username) && (valueMail === variable.email) && (valuePassword === variable.password)) {
+                itemUser.style.border = '3px solid green';
+                itemMail.style.border = '3px solid green';
+                itemPassword.style.border = '3px solid green';
+                alertify.set('notifier', 'position', 'top-center');
+                alertify.success('Merhaba ' + variable.username, 3);
+            } else if ((valueUser !== variable.username) && (valueMail === variable.email) && (valuePassword === variable.password)) {
+                itemUser.style.border = '3px solid red';
+                itemMail.style.border = '3px solid green';
+                itemPassword.style.border = '3px solid green';
+            } else if ((valueUser === variable.username) && (valueMail !== variable.email) && (valuePassword === variable.password)) {
+                itemUser.style.border = '3px solid green';
+                itemMail.style.border = '3px solid red';
+                itemPassword.style.border = '3px solid green';
+            } else if ((valueUser === variable.username) && (valueMail === variable.email) && (valuePassword !== variable.password)) {
+                itemUser.style.border = '3px solid green';
+                itemMail.style.border = '3px solid green';
+                itemPassword.style.border = '3px solid red';
+            }else if (valuePassword.length < 8) {
+                document.getElementById('pswText').style.display = 'flex';
+                itemPassword.style.border = '3px solid red';
+            }
+        });
+
+    }
+
+    onClikHandle = (event) => {
+        alertify.set('notifier', 'position', 'top-center');
+        alertify.error(':)', 3);
+    }
+
+
+    render() {
+        return (
+            <div className='main'>
+                <form onSubmit={this.onSubmitHandle} className='formContainer' name='form1'>
+
+                    <div className='image'>
+                        <lottie-player src="https://assets8.lottiefiles.com/packages/lf20_1pxqjqps.json" background="transparent" speed="1" loop autoplay></lottie-player>
+                    </div>
+                    <div className='container'>
+                        <div className='loginText'>
+                            <h3>{this.props.loginTitles.loginTitle}</h3>
+                        </div>
+                        <label>{this.props.loginTitles.usernameTitle}</label>
+                        <input type="text" id="userName" name="userName" placeholder="Kullanıcı Adı.."></input>
+                        <h6 id='emptyUsername'>{this.props.loginTitles.usernameLetter}</h6>
+
+
+                        <label >{this.props.loginTitles.mailTitle}</label>
+                        <input type="email" id="email" name="email" placeholder="E-posta..."></input>
+                        <h6 id='emptyMail'>{this.props.loginTitles.mailLetter}</h6>
+
+
+
+                        <label >{this.props.loginTitles.passwordTitle}</label>
+                        <input type="password" id="password" name="password" placeholder="Parola.."></input>
+                        <h6 id='pswText'>{this.props.loginTitles.pswLetter}</h6>
+
+
+                        <input onClick={this.onClikHandle} className='submit' type="submit" value="Giriş"></input>
+                        <div className='loginText2'>
+                            <h3>Hesabın Yok mu?</h3>
+                            <a href='/'> Kayıt Ol </a>
+                        </div>
+                    </div>
+
+                </form>
+            </div>
+        )
+    }
+}
